fece28de3384161a8a0d5c57292db9568ce52f2e
Hardik Vatsal Panchal <hardikvp94@gmail.com>
2022-04-21 11:02:46 +0530

    Update connect.php

--- Chat Forum/connect.php
+++ Chat Forum/connect.php
@@ -1,9 +1,9 @@
 <?php
 $server = 'localhost';
 $username   = 'root';
-$password   = 'pvh.3994';
+$password   = '';
 $database   = 'forum';
-$con = mysqli_connect('localhost', 'root', 'pvh.3994');
+$con = mysqli_connect('localhost', 'root', '');
  
 if(!mysqli_connect($server, $username,  $password))
 {
